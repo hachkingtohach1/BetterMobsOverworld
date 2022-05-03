57a77413486c88aea97519a7914c2e2c08711180
MadoxMC <89165041+xodamcm@users.noreply.github.com>
2022-05-03 14:29:26 +0200

    Update Registrations.php

--- src/tgwaste/Mobs/Registrations.php
+++ src/tgwaste/Mobs/Registrations.php
@@ -31,11 +31,11 @@ use tgwaste\Mobs\Entities\Guardian;
 use tgwaste\Mobs\Entities\Horse;
 use tgwaste\Mobs\Entities\Husk;
 use tgwaste\Mobs\Entities\Goat;
-//use tgwaste\Mobs\Entities\IronGolem;
+use tgwaste\Mobs\Entities\IronGolem;
 use tgwaste\Mobs\Entities\Llama;
 //use tgwaste\Mobs\Entities\MagmaCube;
 use tgwaste\Mobs\Entities\MobsEntity;
-//use tgwaste\Mobs\Entities\Mooshroom;
+use tgwaste\Mobs\Entities\Mooshroom;
 use tgwaste\Mobs\Entities\Ocelot;
 use tgwaste\Mobs\Entities\Parrot;
 use tgwaste\Mobs\Entities\Phantom;
@@ -45,9 +45,9 @@ use tgwaste\Mobs\Entities\PufferFish;
 use tgwaste\Mobs\Entities\Rabbit;
 use tgwaste\Mobs\Entities\Salmon;
 use tgwaste\Mobs\Entities\Sheep;
-//use tgwaste\Mobs\Entities\Silverfish;
+use tgwaste\Mobs\Entities\Silverfish;
 use tgwaste\Mobs\Entities\Skeleton;
-//use tgwaste\Mobs\Entities\SkeletonHorse;
+use tgwaste\Mobs\Entities\SkeletonHorse;
 use tgwaste\Mobs\Entities\Slime;
 use tgwaste\Mobs\Entities\Spider;
 use tgwaste\Mobs\Entities\Squid;
@@ -96,10 +96,10 @@ class Registrations {
 			"Guardian" => Guardian::class,
 			"Horse" => Horse::class,
 			"Husk" => Husk::class,
-			//"IronGolem" => IronGolem::class,
+			"IronGolem" => IronGolem::class,
 			"Llama" => Llama::class,
 			//"MagmaCube" => MagmaCube::class,
-			//"Mooshroom" => Mooshroom::class,
+			"Mooshroom" => Mooshroom::class,
 			"Ocelot" => Ocelot::class,
 			"Parrot" => Parrot::class,
 			"Phantom" => Phantom::class,
@@ -109,9 +109,9 @@ class Registrations {
 			"Rabbit" => Rabbit::class,
 			"Salmon" => Salmon::class,
 			"Sheep" => Sheep::class,
-			//"Silverfish" => Silverfish::class,
+			"Silverfish" => Silverfish::class,
 			"Skeleton" => Skeleton::class,
-			//"SkeletonHorse" => SkeletonHorse::class,
+			"SkeletonHorse" => SkeletonHorse::class,
 			"Slime" => Slime::class,
 			"Spider" => Spider::class,
 			"Squid" => Squid::class,
